157d131d6a5c7e728de8ec1ef9abc1ccc2db1263
jannatul114 <jfmithila114@gmail.com>
2022-09-10 14:19:14 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './Banner';
+import Section2 from './Section2';
+import Services from './Services';
+import Specialist from './Specialist';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            <Section2 />
+            <Services />
+            <Specialist />
+        </div>
+    );
+};
+
+export default Home;
